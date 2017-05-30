dde1418f404fe2b1f96d9da0a1b0ea79490a1118
Hans Permana <hans.permana@gmail.com>
2017-05-30 15:41:56 +0200

    include error checking in describeProcess-bc.sh

--- src/main/resources/bin/describeProcess-bc.sh
+++ src/main/resources/bin/describeProcess-bc.sh
@@ -6,14 +6,22 @@
 rootdir=$(dirname $(dirname $0))
 descriptor=$1
 
+function error_exit
+{
+	echo
+	echo "${1:-"Unknown Error"}" 1>&2
+	echo
+	exit 1
+}
+
 if [ "$descriptor" = "" ] ; then
-    echo "usage : $(basename $0) <descriptor file>"
-    exit 1
+	echo "invalid descriptor file"
+	error_exit "usage : $(basename $0) <descriptor file>"
 fi
 
 packagedir=$(dirname $descriptor)
 
-packagename=$(xmlstarlet sel -t -v "/utep:descriptor/utep:processor/utep:packaging/utep:name" $descriptor)
+packagename=$(xmlstarlet sel -t -v "/utep:descriptor/utep:processor/utep:packaging/utep:name" $descriptor | tr ' ' '_')
 packageversion=$(xmlstarlet sel -t -v "/utep:descriptor/utep:processor/utep:packaging/utep:version" $descriptor)
 processorname=$(xmlstarlet sel -t -v "/utep:descriptor/utep:processor/utep:name" $descriptor)
 
@@ -22,4 +30,33 @@ PROCESSOR_ID=urbantep-${packagename}~${packageversion}~${processorname}
 read -p "Enter User Name: " WPS_USER
 read -s -p "Enter Password: " PASSWORD
 
-wget --user=$WPS_USER --password=$PASSWORD -q -O - "bc-wps:9080/bc-wps/wps/calvalus?Service=WPS&Request=DescribeProcess&Version=1.0.0&Identifier=$PROCESSOR_ID" | xsltproc $rootdir/etc/describeProcess.xsl -
+describeProcessResponse=$(wget --user=$WPS_USER --password=$PASSWORD -q -O - "www.brockmann-consult.de/bc-wps/wps/calvalus?Service=WPS&Request=DescribeProcess&Version=1.0.0&Identifier=$PROCESSOR_ID")
+commandResponse=$?
+if [ $commandResponse -eq 0 ]; then
+	if [[ $describeProcessResponse == *"<ows:Exception exceptionCode="*  ]]; then
+		if [[ $describeProcessResponse == *"Unable to retrieve the selected process(es)"* ]] || [[ $describeProcessResponse == *"Unable to retrieve processor"* ]]; then
+			error_exit "The processor is not available"
+		elif [[ $describeProcessResponse == *"Unable to retrieve the selected process(es)"* ]]; then
+			error_exit "No processor name is specified"
+		else
+			error_exit "An error is returned by the WPS"
+		fi
+	elif [[ $describeProcessResponse == "" ]]; then
+		error_exit "Empty response from WPS"
+	else
+		continue
+	fi
+elif [ $commandResponse -eq 4 ]; then
+	error_exit "Unable to connect to WPS server."
+elif [ $commandResponse -eq 6 ]; then
+	error_exit "Invalid username/password."
+else
+	error_exit
+fi
+
+echo "$describeProcessResponse" | xsltproc $rootdir/etc/describeProcess.xsl -
+
+if [ "$?" != 0 ]; then
+	error_exit "unable to parse the DescribeProcess response."
+fi
+
